9b3ef6b39d40b8922d54d63905870f31bf23c400
SimarpreetSingh226 <ssimarpreet002@gmail.com>
2026-02-27 18:01:42 +0530

    Sidebar Completion

--- Client/src/Components/Sidebar.jsx
+++ Client/src/Components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useAppContext } from "../Context/AppContext";
 import { assets } from "../assets/assets";
+import moment from "moment";
 
 const Sidebar = () => {
-  const { chats, setSelected, theme, setTheme, User } = useAppContext();
+  const { chats, setSelected, theme, setTheme, user, navigate } =
+    useAppContext();
   const [search, setsearch] = useState("");
 
   return (
@@ -52,10 +54,71 @@ const Sidebar = () => {
                     ? chat.messages[0].content.slice(0, 32)
                     : chat.name}
                 </p>
-                <p>{chat.updatedAt}</p>
+                <p className="text-xs text-gray-500 dark:text-[#b14a6c0]">
+                  {moment(chat.updatedAt).fromNow()}
+                </p>
               </div>
+              <img
+                src={assets.bin_icon}
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+                alt=""
+              />
             </div>
           ))}
+      </div>
+      {/* Community Images */}
+      <div
+        onClick={() => {
+          navigate("/community");
+        }}
+        className="flex items-center gap-4 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all "
+      >
+        <img
+          src={assets.gallery_icon}
+          className="w-4.5 not-dark:invert"
+          alt=""
+        />
+        <div className="flex flex-col text-sm ">
+          <p>Community Images</p>
+        </div>
+      </div>
+
+      {/* Credit Purchase Options */}
+      <div
+        onClick={() => {
+          navigate("/credits");
+        }}
+        className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all "
+      >
+        <img
+          src={assets.diamond_icon}
+          className="w-4.5 not-dark:invert"
+          alt=""
+        />
+        <div className="flex flex-col text-sm ">
+          <p>Credits : {user?.credits} </p>
+          <p className="text-xs text-gray-400 ">
+            Purchase credits to use QuickGPT
+          </p>
+        </div>
+      </div>
+
+      {/* dark mode toggle */}
+      <div className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md">
+        <div className="flex items-center gap-2 text-sm ">
+          <img src={assets.theme_icon} className="w-4 not-dark:invert" alt="" />
+          <p>Dark Mode</p>
+        </div>
+        <label className="relative inline-flex cursor-pointer">
+          <input
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+            type="checkbox"
+            className="sr-only peer"
+            checked={theme === "dark"}
+          />
+          <div className="w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all"></div>
+          <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
+        </label>
       </div>
     </div>
   );
--- Client/src/Context/AppContext.jsx
+++ Client/src/Context/AppContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyChats, dummyUserData } from "../assets/assets";
+import { assets, dummyChats, dummyUserData } from "../assets/assets";
+import moment from "moment";
+import React from "react";
 
 const AppContext = createContext();
 
@@ -26,6 +28,7 @@ export const AppContextProvider = ({ children }) => {
     } else {
       document.documentElement.classList.remove("dark");
     }
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   useEffect(() => {
@@ -51,9 +54,86 @@ export const AppContextProvider = ({ children }) => {
     selectedchat,
     setSelectedchat,
     theme,
+    setTheme,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => useContext(AppContext);
+export const Sidebar = () => {
+  const { chats, setSelected, theme, setTheme, User } = useAppContext();
+  const [search, setsearch] = useState("");
+
+  return (
+    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-linear-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3x tarnsition-all duration-500 max-md:absolute left-0 z-1">
+      {/* logo */}
+      <img
+        src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt=""
+        className="w-full max-w-48"
+      />
+      {/* new chat button */}
+      <button className="flex justify-center items-center w-full py-2 mt-10 text-white bg-linear-to-r from-[#a456f7] to-[#3081f6] text-sm rounded-md cursor-pointer ">
+        <span className="mr-2 text-xl">+</span> New Chat
+      </button>
+      {/* search conservation */}
+      <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md">
+        <img src={assets.search_icon} className="w-4 not-dark:invert" alt="" />
+        <input
+          onChange={(e) => setsearch(e.target.value)}
+          value={search}
+          type="text"
+          placeholder="Search Conversations"
+          className="text-xs placeholder:text-gray-400 outline-none"
+        />
+      </div>
+
+      {/* recentchat */}
+      {chats.length > 0 && <p className="mt-4 text-sm">recent Chats</p>}
+      <div className="flex-1 overflow-y-scroll mt-3 text-sm space-y-3">
+        {chats
+          .filter((chat) =>
+            chat.messages[0]
+              ? chat.messages[0]?.content
+                  .toLowerCase()
+                  .includes(search.toLowerCase())
+              : chat.name.toLowerCase().includes(search.toLowerCase()),
+          )
+          .map((chat) => (
+            <div
+              key={chat._id}
+              className="p-2 px-4 dark:bg-[#57317c]/10 border border-gray-300 dark:border-[#80609f]/15 rounded-md cursor-pointer flex justify-between group"
+            >
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0
+                    ? chat.messages[0].content.slice(0, 32)
+                    : chat.name}
+                </p>
+                <p className="text-xs text-gray-500 dark:text-[#b14a6c0]">
+                  {moment(chat.updatedAt).fromNow()}
+                </p>
+              </div>
+              <img
+                src={assets.bin_icon}
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+                alt=""
+              />
+            </div>
+          ))}
+      </div>
+      {/* Community Images */}
+      <div className="flex items-center gap-4 p-3 mt-4 border border-gray-500 ">
+        <img
+          src={assets.gallery_icon}
+          className="w-4.5 not-dark:invert"
+          alt=""
+        />
+        <div className="flex flex-col text-sm ">
+          <p>Community Images</p>
+        </div>
+      </div>
+    </div>
+  );
+};
